9096a0ceeddfb03061bccb1d44ee15b2a7f26e14
msimbo-coding <SAlmonte@MSIMBO.org>
2021-02-16 13:47:36 -0500

    imperial-b repo redo

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        'blue-gray': colors.blueGray,
+        'cool-gray': colors.coolGray,
+        gray: colors.gray,
+        'true-gray': colors.trueGray,
+        'warm-gray': colors.warmGray,
+        red: colors.red,
+        orange: colors.orange,
+        amber: colors.amber,
+        yellow: colors.yellow,
+        lime: colors.lime,
+        green: colors.green,
+        emerald: colors.emerald,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        'light-blue': colors.lightBlue,
+        blue: colors.blue,
+        indigo: colors.indigo,
+        violet: colors.violet,
+        purple: colors.purple,
+        fuchsia: colors.fuchsia,
+        pink: colors.pink,
+        rose: colors.rose,
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
